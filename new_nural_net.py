5fa0305886a9418feb6fad2195a5f71a15050fd5
Xingyu (Peter) He <xhe@mac-21830.hamilton.edu>
2018-04-17 18:31:48 -0400

    created my own net

---
+++ new_nural_net.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# import dataset
+def sigmoid(z):
+    return 1 / ( 1 + np.exp(-z))
+def sigmoid_gradient(z):
+    return sigmoid(z) * (1 - sigmoid(z))
+
+def tanh(z):
+    return (np.exp(z) - np.exp(-z)) / (np.exp(z) + np.exp(-z))
+
+def tanh_gradient(z):
+    return 1 - np.power(tanh(z),2)
+
+# process dataset for models
+# vectorize data
+
+def load_dataset():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+
+
+## START CODE HERE ## (PUT YOUR IMAGE NAME)
+my_image = "my_image.jpg"   # change this to the name of your image file
+## END CODE HERE ##
+
+# We preprocess the image to fit your algorithm.
+fname = "images/" + my_image
+image = np.array(ndimage.imread(fname, flatten=False))
+my_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((1, num_px*num_px*3)).T
+my_predicted_image = predict(d["w"], d["b"], my_image)
+
+plt.imshow(image)
+print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
